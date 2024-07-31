be4bb0c00cd6a90e2e594c3726b9bed5a28698e3
soljal543 <soljal543@gmail.com>
2024-07-31 15:45:22 +0000

    off debugbanner

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: HomePage()
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
